37ff5aba00570628040026357b9529a295ed050b
Daniel Chamot <daniel@nullkarma.com>
2017-04-21 17:20:17 +0200

    alert_aggregation

--- actions/alert_aggregation.py
+++ actions/alert_aggregation.py
@@ -3,10 +3,11 @@
 from st2actions.runners.pythonrunner import Action
 from collections import defaultdict
 
-class AlertAggregateCheck(object):
-    def run(self, alerts, node_label, check_label):
+
+class AlertAggretation(object):
+    def run(self, alerts, outer_label, inner_label):
         a = defaultdict(list)
         for alert in alerts:
-            a[alert['labels'][node_label]].append(alert['labels'][check_label])
+            a[alert['labels'][outer_label]].append(alert['labels'][inner_label])
 
         return a
